2c5e8ab9539104579774fa0451c930ccfcba41bb
guillermo vicente prieto <gvicenteprieto@gmail.com>
2022-07-07 16:15:19 -0300

    preparando 3 desafio v.5

--- src/database.js
+++ src/database.js
@@ -3,17 +3,13 @@ import logger from "./utils/loggers.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const mongoDB_URI = process.env.MONGO_URI || "mongodb://localhost:27017/3E_DB";
+//const mongoDB_URI = process.env.MONGO_URI || "mongodb://localhost:27017/3E_DB";
 
-//const mongoDB_URI =  "mongodb://localhost:27017/3E_DB";
+const mongoDB_URI =  "mongodb://localhost:27017/3E_DB";
 
 (async () => {
     try {
-        const db = mongoose.connect(mongoDB_URI
-            , {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            })
+        const db = await mongoose.connect(mongoDB_URI)
         logger.info('Conectado a mongoDB ' + db.connection.host + ':' + db.connection.port + '/' + db.connection.name);
     } catch (error) {
         logger.error(error);
